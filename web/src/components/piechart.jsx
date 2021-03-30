4aab227dfab3fa421a0831d410942aa22cadc55d
Katrina Dutton <75206692+kdutton891@users.noreply.github.com>
2021-03-30 15:31:34 +0100

    Colour change

--- web/src/components/piechart.jsx
+++ web/src/components/piechart.jsx
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
-
 const data = [
   { name: 'Exercise', value: 400 },
   { name: 'Sleep', value: 300 },
@@ -10,18 +9,19 @@ const data = [
   { name: 'Group F', value: 189 },
 ];
 
+
 export const ChartX = () => {
     return (
-        <PieChart width={400} height={400}>
+        <PieChart width={600} height={600}>
           <Pie
             dataKey="value"
             startAngle={180}
             endAngle={0}
             data={data}
-            cx="200px"
-            cy="200px"
+            cx="600px"
+            cy="600px"
             outerRadius={80}
-            fill="#8884d8"
+            fill="#2C4E81"
             label
           />
         </PieChart>
